0f641db6e12a57e0847657950ce461d2a6c6aba5
diegoram <diego@diegoram.me>
2016-11-01 18:09:21 -0300

    add more detailed test to debug

--- src/test/scala/org/challenge/diegoram/challengeSpec.scala
+++ src/test/scala/org/challenge/diegoram/challengeSpec.scala
@@ -6,10 +6,13 @@ import org.specs2.specification.core.SpecStructure
 class ChallengeSpec extends Specification {
 
   override def is: SpecStructure = s2"""
-    find all rating for a Movie By Title $finAllRatingForAMovie
-    find average rating for a movie by title $findAverageForMovie
-    find user favourite gender my name $failure
-    find recomendations for user XXX $failure
+    find all rating for a Movie By Title        $finAllRatingForAMovie
+    find average rating for a movie by title    $findAverageForMovie
+    find user 1 by id                           $failure
+    find all rated movies for user 1            $failure
+    find all genre Seq for a given movie        $failure
+    find user favourite gender my name          $findFavouriteGenderForUser
+    find recomendations for user XXX            $failure
 
     """
 
@@ -27,14 +30,18 @@ class ChallengeSpec extends Specification {
     MovieDao.averageRating("Toy Story (1995)") must beEqualTo(Some(3.0))
   }
 
-  def findFavouriteGenderForUser = {
-    UserDao.findOne(_.id == 1) match {
+  def findFavouriteGenderForUser =
+    (UserDao.findOne(_.id == 1) match {
       case Some(user) => {
-        RatingDao.findAll(_.userId == user.id).sortBy(_.rating).reverse.take(10).map { rat =>
-          MovieDao.findById(rat.movieId).map( mov => MovieDao.getGenreArray(mov.genres))
-        }
+        (for {
+          rat <- RatingDao.findAll(_.userId == user.id).sortBy(_.rating).reverse.take(10)
+          movie = MovieDao.findById(rat.movieId)
+          genre <- MovieDao.getGenreArray(movie.get.genres)
+            if movie.isDefined
+        } yield genre).groupBy(_.name).maxBy(_._2.size)._1
       }
-    }
-  }
+      case None => ""
+    }) must beEqualTo("Horror")
+
 
 }
