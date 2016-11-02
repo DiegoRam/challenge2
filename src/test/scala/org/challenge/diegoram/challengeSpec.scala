31069c61b838780640d8ef916f842ae988944600
diegoram <diego@diegoram.me>
2016-11-02 15:22:34 -0300

    fixed bug on test

--- src/test/scala/org/challenge/diegoram/challengeSpec.scala
+++ src/test/scala/org/challenge/diegoram/challengeSpec.scala
@@ -9,7 +9,7 @@ class ChallengeSpec extends Specification {
     find all rating for a Movie By Title        $finAllRatingForAMovie
     find average rating for a movie by title    $findAverageForMovie
     find user favourite gender by name          $findFavouriteGenderForUser
-    find recomendations for user XXX            $findRecomendationsForUser
+    find recomendations for user 345            $findRecomendationsForUser
 
     """
 
@@ -42,21 +42,36 @@ class ChallengeSpec extends Specification {
     }) mustEqual Some(Genre(8,"Drama"))
 
 
+  def movieMatch(a: Seq[Movie], b: Seq[Movie]): Boolean = {
+    def loop(xs: Seq[Movie], sample: Seq[Movie], count: Int): Boolean = {
+      if(count >=5) true
+      else{
+        xs match {
+          case Nil => count >=5
+          case head::tail => loop(tail, sample, if(sample.contains(head)) count + 1 else count)
+        }
+      }
+    }
+    loop(a,b,0)
+  }
+
   def findRecomendationsForUser = {
     val myTopRatedMovies =
       for{
-        rating <- RatingDao.findAll(rat => rat.userId == 98 && rat.rating >= 4).take(10)
+        rating <- RatingDao.findAll(rat => rat.userId == 345 && rat.rating >= 4).take(10)
         movie <- MovieDao.findById(rating.movieId)
       } yield movie
 
     val recommended =
       (for {
-        otherRat <- RatingDao.findAll(ot => ot.rating >=4 && myTopRatedMovies.exists(_.id == ot.movieId)).take(10)
-        user <- RatingDao.findAll(_.userId == otherRat.userId).take(10)
-        newMov <- MovieDao.findById(user.movieId)
+        otherUserRat <- RatingDao.findAll(ot => ot.rating >=4 && myTopRatedMovies.exists(_.id == ot.movieId))
+          .take(10).map(_.userId)
+        userRat <- RatingDao.findAll(ot => ot.rating >=4 && otherUserRat == ot.userId)
+        newMov <- MovieDao.findById(userRat.movieId)
       } yield newMov)
         .toSet
+        .filterNot(myTopRatedMovies.contains)
 
-    recommended.size must beGreaterThan(0)
+    recommended must haveSize(488)
   }
 }
